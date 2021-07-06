88f2fecf8b1e6fc5c95ea10ac1a60514a4855ff9
Kartik Sapra <36664705+theexplorist@users.noreply.github.com>
2021-07-06 22:22:42 +0530

    Create HelloJava.java

---
+++ Session1/HelloJava.java
@@ -0,0 +1,12 @@
+package Session1;
+
+public class HelloJava {
+
+	public static void main(String[] args) { //entrypoint
+		// TODO Auto-generated method stub
+		System.out.print("Hello amigos!");
+		System.out.println("Hello friends!");
+		System.out.println("Lets do java");
+	}
+
+}
