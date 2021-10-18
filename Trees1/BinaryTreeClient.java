7d8d35ef3784d63e7a11761304396854d43fbdb1
Kartik Sapra <36664705+theexplorist@users.noreply.github.com>
2021-10-18 12:26:41 +0530

    Create BinaryTreeClient.java

---
+++ Trees1/BinaryTreeClient.java
@@ -0,0 +1,14 @@
+package Trees1;
+
+public class BinaryTreeClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BinaryTree bt = new BinaryTree();
+		//System.out.println(bt.root);
+		//bt.display();
+		bt.postOrder();
+	}
+
+}
